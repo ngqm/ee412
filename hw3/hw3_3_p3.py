8cef195f472b8e0e79a0a54712ecdb2fb3747ecc
Quang Minh Nguyen <qm.nguyen@kaist.ac.kr>
2022-11-18 02:35:13 +0900

    grid search svm

--- hw3/hw3_3_p3.py
+++ hw3/hw3_3_p3.py
@@ -12,10 +12,7 @@ from time import time
 
 
 # hyperparameters
-C = .1 # regularisation parameter 
-ETA = 0.002 # learning rate
-N_ITERS = 1000 # number of iterations
-TOL = 1e-4 # tolerance for convergence
+N_ITERS = 100 # number of iterations
 
 
 def preprocess(features, labels):
@@ -41,6 +38,10 @@ def preprocess(features, labels):
             of the corresponding data point
     """
     X = np.loadtxt(features, delimiter=",")
+    # deletes zero columns from X
+    X = X[:, np.any(X, axis=0)]
+    # normalises the data
+    X = (X - np.mean(X, axis=0))/np.std(X, axis=0)
     X = np.hstack((X, np.ones((X.shape[0], 1))))
     Y = np.loadtxt(labels)
     return X, Y
@@ -60,24 +61,16 @@ def train(X, Y, c, eta):
     Returns:
         W: ndarray, the weights of the SVM
     """
-    W_cur = 0.01*np.random.rand(X.shape[1])
-    W = W_cur
-    # W = np.zeros(X.shape[1])
-    # for _ in range(N_ITERS):
-    print('Training...')
+    W = 0.01*np.random.randn(X.shape[1])
     for _ in range(N_ITERS):
         # computes gradient
-        grad = np.zeros(X.shape[1])
+        gradient = np.zeros(X.shape[1])
         for i in range(len(X)):
             if Y[i]*(np.dot(W, X[i])) < 1:
-                grad -= Y[i]*X[i]
-        grad = W + c*grad
-        # update weights
-        W -= eta*grad
-        # gets the train set accuracy
-        predictions = predict(W, X)
-        acc = accuracy(predictions, Y)
-        print('ACC = {}'.format(acc))
+                gradient -= Y[i]*X[i]
+        gradient = W + c*gradient
+        # updates weights
+        W -= eta*gradient
     return W
 
 
@@ -90,6 +83,10 @@ def predict(W, X):
         W: ndarray, the weights of the SVM
         X: ndarray, each row is a data point
             augmented by a 1
+
+    Returns:
+        Y: ndarray, each entry is the predicted
+            label of the corresponding data point
     """
     Y = np.sign(np.dot(X, W))
     return Y
@@ -127,56 +124,82 @@ def get_cross_validation_accuracy(X, Y, c, eta):
         acc: float, the cross validation accuracy
     """
 
-    # shuffles the data
+    # shuffles data
     indices = np.arange(len(X))
     np.random.shuffle(indices)
     X = X[indices]
     Y = Y[indices]
 
-    # splits the data into 10 folds
+    # splits data into 10 folds
     X_folds = np.split(X, 10)
     Y_folds = np.split(Y, 10)
 
-    # computes the cross validation accuracy
+    # computes cross validation accuracy
     acc = 0
     for i in range(10):
-        # gets the training data
-        X_train = np.concatenate([X_folds[j] for j in range(10) if j != i])
-        Y_train = np.concatenate([Y_folds[j] for j in range(10) if j != i])
-        # gets the validation data
+        # gets training folds
+        X_train = np.concatenate(X_folds[:i] + X_folds[i+1:])
+        Y_train = np.concatenate(Y_folds[:i] + Y_folds[i+1:])
+        # gets validation fold
         X_val = X_folds[i]
         Y_val = Y_folds[i]
-        # trains the SVM and get weights
+        # trains SVM and gets weights
         W = train(X_train, Y_train, c, eta)
-        # computes the accuracy
+        # computes accuracy
         predictions = predict(W, X_val)
         acc += accuracy(predictions, Y_val)
     acc /= 10
     return acc
 
 
-def grid_search():
+def grid_search(X, Y):
     """
-    Perform grid search to find the best hyperparameters
+    Perform grid search to find the best hyperparameters.
+
+    Returns:
+        best_c: float, the best regularisation parameter
+        best_eta: float, the best learning rate
+        best_acc: float, the best cross validation accuracy
     """
     
     # hyperparameters
-    Cs = np.arange(0.1, 1.1, 0.1)
-    ETAs = np.arange(0.0001, 0.0011, 0.0001)
+    Cs      = np.arange(0.1, 1, 0.2)  # 5 values 
+    ETAs    = np.arange(0.0001, 0.0011, 0.0001)  # 10 values
     # best instance
     best_acc = 0
     best_c = 0
     best_eta = 0
-    # performs grid search
-    for c in Cs:
-        for eta in ETAs:
-            acc = get_cross_validation_accuracy(X, Y, c, eta)
-            print('C = {}, ETA = {}, ACC = {}'.format(c, eta, acc))
-            if acc > best_acc:
-                best_acc = acc
-                best_c = c
-                best_eta = eta
-    print('Best C = {}, Best ETA = {}, Best ACC = {}'.format(best_c, best_eta, best_acc))
+    # grid search
+    with open('grid_search_normal.txt', 'w') as f:
+        for c in Cs:
+            for eta in ETAs:
+                acc = get_cross_validation_accuracy(X, Y, c, eta)
+                f.write('{:.1f}\t{:.4f}\t{:.4f}\n'.format(c, eta, acc))
+                if acc > best_acc:
+                    best_acc = acc
+                    best_c = c
+                    best_eta = eta
+    return best_c, best_eta, best_acc
+
+
+# def plot_grid_search():
+#     """
+#     Plot the grid search results.
+#     """
+#     import matplotlib.pyplot as plt
+
+#     # reads data
+#     data = np.loadtxt('grid_search_normal.txt')
+#     Cs, ETAs, ACCs = data[:, 0], data[:, 1], data[:, 2]
+#     ACCs = ACCs.reshape((5, 10))
+#     plt.imshow(ACCs)
+#     plt.xticks(np.arange(10), ETAs[:10], rotation=45)
+#     plt.yticks(np.arange(5), Cs[::10])
+#     plt.xlabel('eta')
+#     plt.ylabel('c')
+#     plt.title('Cross validation accuracy')
+#     plt.colorbar()
+#     plt.savefig('grid_search_normal.png')
 
 
 if __name__=='__main__':
@@ -184,9 +207,8 @@ if __name__=='__main__':
     features = sys.argv[1]
     labels = sys.argv[2]
     X, Y = preprocess(features, labels)
-    # X, Y = X[:500], Y[:500]
-    error = get_cross_validation_accuracy(X, Y, 1, 0.001)
-    print(error)
-    # grid_search()
-
-
+    best_c, best_eta, best_acc = grid_search(X, Y)
+    print(best_acc)
+    print(best_c)
+    print(best_eta)
+    # plot_grid_search()
